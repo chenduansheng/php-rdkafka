bd2221ca76bdef70652afe180929fc656b4b0fa3
chenduansheng <476210724@qq.com>
2021-06-23 18:27:06 +0800

    xx

---
+++ example/producer2.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../src/Common/MQ/Kafka/NmredKafkaProducer.php';
+require_once '../nmred/kafka-php/src/Kafka/ProducerConfig.php';
+
+$config = [
+    'brokerList'=>'192.168.59.100:9093, 192.168.59.100:9094, 92.168.59.100:9095',
+    'clientId'=>'',
+    'drCbLogPath'=>'d:/kafkaProducerDrLog',
+    'errCbLogPath'=>'d:/kafkaProducerErrLog',
+    'ack'=>1
+];
+
+( \Stary\Common\MQ\Kafka\NmredKafkaProducer::instance('test2', $config) )->send('A message for test');
